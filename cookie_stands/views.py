48fa2ff9c4f6b47ec0f06893c172a592c3d4f742
Mmarcos01 <marie.marcos25@gmail.com>
2021-09-18 15:55:05 -0700

    postgres

--- cookie_stands/views.py
+++ cookie_stands/views.py
@@ -1,7 +1,7 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import CookieStandSerializer
 from .models import CookieStand
-# from .permissions import IsOwnerOrReadOnly
+from .permissions import IsOwnerOrReadOnly
 # from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class CookieStandList(ListCreateAPIView):
@@ -10,6 +10,6 @@ class CookieStandList(ListCreateAPIView):
     serializer_class = CookieStandSerializer
 
 class CookieStandDetail(RetrieveUpdateDestroyAPIView):
-    # permission_classes = (IsOwnerOrReadOnly,)
+    permission_classes = (IsOwnerOrReadOnly,)
     queryset = CookieStand.objects.all()
     serializer_class = CookieStandSerializer
